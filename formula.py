d557d489e81be7fb8d62220e26a8cb25d9be9511
2knn10 <109735963+2knn10@users.noreply.github.com>
2022-09-26 15:11:35 +0300

    Create formula.py

---
+++ formula.py
@@ -0,0 +1,155 @@
+
+style = '''\033[35m
+_______ _ _ _______    _      ______                         _                         _             
+ |__   __(_) |__   __|  | |    |  ____|                       | |                       | |            
+    | |   _| | _| | ___ | | __ | |__ ___  _ __ _ __ ___  _   _| | __ _   _ __ ___   __ _| | _____ _ __ 
+    | |  | | |/ / |/ _ \| |/ / |  __/ _ \| '__| '_ ` _ \| | | | |/ _` | | '_ ` _ \ / _` | |/ / _ \ '__|                  
+    | |  | |   <| | (_) |   <  | | | (_) | |  | | | | | | |_| | | (_| | | | | | | | (_| |   <  __/ |   
+    |_|  |_|_|\_\_|\___/|_|\_\ |_|  \___/|_|  |_| |_| |_|\__,_|_|\__,_| |_| |_| |_|\__,_|_|\_\___|_|   
+   _   
+ _| |_ 
+|_   _|
+  |_|  
+       
+                                                                                                       
+                                                                                                       
+
+|=================================================|
+|      [ + ] by kivani                            |
+|=================================================|
+|      [ + ] Instagram : 2knn01                   |                   
+|=================================================|
+|      [ + ] Telegram : https://t.me/kavani2knn10 |                   
+|=================================================|
+'''
+print(style)
+
+
+
+
+style = '''\033[37m
+==========================================
+ - prime formula   [1]\n 
+==========================================
+ - second formula  [2]\n 
+==========================================
+ - third formula   [3]\n
+==========================================
+ - Fourth formula  [4]\n                     
+==========================================
+'''
+print(style)
+
+choose = input("Please choose an option : ")
+if choose == '1':
+        print("=================================================")
+        print("Enter the victim's information")
+        print("=================================================")      
+        user = input('user : ')	
+        date = input('date: ')       
+        location = input('location : ')
+        login  = input('Recent : ')
+        phone  = input('Used phone: ')
+        Email = input("Used email : ")
+        print("=================================================")	
+        print("Hey there TikTok support,I want to recover my account") 
+        print("The following information is my info ")   
+        print("Username @ : "+user)
+        print("Sign-up date : "+date)
+        print("Recent login location : "+location)      
+        print("Recent login : "+login)
+        print("Used phone : "+phone)
+        print("Email : "+Email)
+        
+        print("=================================================")        
+        style = '''\033[31m
+        (Copy and paste it into Tik Tok support)       
+        '''
+        print(style) 
+        
+  
+     
+
+
+if choose == '2':
+        print("=================================================")
+        print("Enter the victim's information")
+        print("=================================================")        
+        user = input("User @ : ")
+        Date = input("Sign Date : ")
+        Location = input("Login Location : ")
+        Device = input("Device : ")
+        number = input("phone number : ")
+        Email = input("Bound/old Email : ")
+        print("=================================================")
+        print("Hi there , I would like to recover my account  , here are my ")
+        print("information for you to make sure that I own the account , ")
+        print("Username @ : "+user)
+        print("Sign-up date : "+Date)
+        print("Registered Device : "+Device)      
+        print("Bound/old phone number : "+number)      
+        print("Email : "+Email)
+        print("Thank you,")
+        print("=================================================")
+        style = '''\033[31m
+        (Copy and paste it into Tik Tok support)       
+        '''
+        print(style)  
+
+
+if choose == '3':        
+        print("=================================================")
+        print("Enter the victim's information")
+        print("=================================================")
+        user = input("User @ : ")
+        Date = input("Sign-up : ")
+        Location = input("Login Location : ")
+        Device = input("Registered : ")
+        number = input("phone number : ")
+        Email = input("Email : ")
+        insta = input("Link Instagram : @ ")
+        print("=================================================")
+        print("Hey there TikTok support, id like recover my account The following information my , ")  
+        print("Username @ : "+user)
+        print("Sign-up date : "+Date)
+        print("Recent login location : "+Location)
+        print("Registered Device : "+Device)      
+        print("Bound/old phone number : "+number)      
+        print("My Email : "+Email)
+        print("My Link Instagram : "+insta)
+        print("your welcome") 
+        print("=================================================")
+        style = '''\033[31m
+        (Copy and paste it into Tik Tok support)       
+        '''
+        print(style)
+    
+       
+ 
+if choose == '4':        
+        print("=================================================")
+        print("Enter the victim's information")
+        print("=================================================")
+        user = input("User @ : ")
+        date = input("Sign-up date : ")
+        Location = input("Recent login location : ")
+        face = input("Facebook : ")
+        email = input("Email : ")
+        Number = input("Phone Number : ")        
+        print("=================================================")
+        print("Hi TikTok, I lost my account and I need to recover the account, here is the information about the account below ")      
+        print("Username @ : "+user)
+        print("Sign-up date : "+date)
+        print("Recent login location : "+Location)
+        print("My Facebook : "+face)                   
+        print("Email : "+email)
+        print("phone number : "+Number) 
+        print("you're welcome")
+        print("=================================================")
+        style = '''\033[31m
+        (Copy and paste it into Tik Tok support)       
+        '''
+        print(style)
+        
+        
+        
